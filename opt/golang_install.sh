78642d082611fda8e63765ca6421de2dc3de59eb
ystkfujii <76274657+ystkfujii@users.noreply.github.com>
2022-02-26 14:13:53 +0900

    Update golang_install.sh

--- opt/golang_install.sh
+++ opt/golang_install.sh
@@ -3,8 +3,9 @@
 # https://golangstart.com/no_apt/
 
 wget https://dl.google.com/go/go1.17.linux-amd64.tar.gz
-tar -C /usr/local -xzf go1.17.linux-amd64.tar.gz
-export PATH=$PATH:/usr/local/go/bin
+mkdir -p ${PATH}/opt
+tar -C ${PATH}/opt -xzf go1.17.linux-amd64.tar.gz
+export PATH=$PATH:${PATH}/go/bin
 rm go1.17.linux-amd64.tar.gz
 go version
 
